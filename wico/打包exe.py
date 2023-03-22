e727efb44e67a6ddf60ef35254665e38f571a8e9
102757017 <sdf63fg@yeah.net>
2023-03-22 16:27:57 +0800

    在kv文件内导入package会导致pyinstaller打包的程序找不到包，因此将import代码转移到py文件内。

---
+++ wico/打包exe.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import subprocess
+
+
+#在64位系统下打包的程序只能在64位系统上运行
+
+
+sys.path.append(os.path.dirname(__file__))
+a="\""+ sys.prefix + "\python.exe\" "  + "\""+ sys.prefix + "\Scripts\pyinstaller.exe\" " +os.path.dirname(__file__)+r"\main.py"+" --distpath="+os.path.dirname(__file__)
+
+#生成command命令,指定安装源为阿里云
+print("command命令是："+ a)
+
+#pipe = subprocess.Popen(a, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True).stdout  
+#print(pipe.read())
+
+os.system(a)
